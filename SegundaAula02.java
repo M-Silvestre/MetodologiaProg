0a5c8cfc907ded0d793e31d9d863ba1998f99de4
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-12 22:33:23 -0200

    Create SegundaAula02.java

---
+++ SegundaAula02.java
@@ -0,0 +1,18 @@
+/**
+ * Aula 02, Programa 02
+* Classe de exemplo com main para exibir a forma como chamar
+* uma classe de outro arquivo
+* Nome: Matteus Silvestre
+* Data: 12 de fev de 2019
+*/
+
+public class Professor{
+    
+    public void ensinar(){
+        System.out.println("Ensinando Java");
+    }
+    
+    public void aprovar(){
+        System.out.println("Aprovando");
+    }
+}
