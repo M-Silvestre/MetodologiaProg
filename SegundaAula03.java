5705eac68ee8e456eee180ef6017decf340b1e6b
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-12 22:34:55 -0200

    Create SegundaAula03.java

---
+++ SegundaAula03.java
@@ -0,0 +1,20 @@
+/**
+ * Aula 02, Programa 03
+* Classe de exemplo com main para exibir a forma como chamar
+* uma classe de outro arquivo
+* Nome: Matteus Silvestre
+* Data: 12 de fev de 2019
+*/
+
+//Se dois arquivos estão numa mesma pasta, é possível chamar
+// métodos de outros arquivos
+
+public class Principal{
+	public static void main(String[] args) {
+	    
+	    //Instanciar um objeto para chamar os métodos
+		Professor prof = new Professor();
+		prof.ensinar();
+		prof.aprovar
+	}
+}
